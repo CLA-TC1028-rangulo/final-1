3601a919e9654ab85fcf1da0b5e2ef58e87fec84
pangulotecmx <88519161+pangulotecmx@users.noreply.github.com>
2022-10-17 16:03:31 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+![Tec de Monterrey](images/logotecmty.png)
+# Ejercicios con estatutos de repetición
+
+
+- 22 Inverso Dígitos
+
+
---
+++ assignments/22InversoDigitos/src/exercise.py
@@ -0,0 +1,8 @@
+def main():
+    num = int(input("Enter a number: "))
+    #escribe tu código abajo de esta línea
+    
+
+
+if __name__=='__main__':
+    main()
